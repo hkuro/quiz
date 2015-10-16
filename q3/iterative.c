709dc44d10c6f201c41c12af7aa33bb1c2512341
hkuro <kuro2357509@gmail.com>
2015-10-16 21:34:49 +0800

    Implement q3 iterative version.
    When I took the exam, I wrote down the interative version.
    Now I prepared the test data(CreateTestTree()) to finished it in C.

--- q3/iterative.c
+++ q3/iterative.c
@@ -1,6 +1,81 @@
 /* FIXME: Implement! */
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct tree_node {
+    int data;
+    struct tree_node *left;
+    struct tree_node *right;
+
+} TreeNode;
+
+/*	TestTree
+		1
+	   / \
+	  2   5
+	 / \   \
+	3  4    6
+*/
+TreeNode *CreateTestTree()
+{
+    TreeNode *node1 = (TreeNode *)malloc(sizeof(TreeNode));
+    node1->data = 1;
+    node1->left = (TreeNode *)malloc(sizeof(TreeNode));
+    node1->right = (TreeNode *)malloc(sizeof(TreeNode));
+
+    TreeNode *node2 = node1->left;
+    node2->data = 2;
+    node2->left = (TreeNode *)malloc(sizeof(TreeNode));
+    node2->right = (TreeNode *)malloc(sizeof(TreeNode));
+
+    TreeNode *node3 = node2->left;
+    node3->data = 3;
+    node3->left = NULL;
+    node3->right = NULL;
+
+    TreeNode *node4 = node2->right;
+    node4->data = 4;
+    node4->left = NULL;
+    node4->right = NULL;
+
+    TreeNode *node5 = node1->right;
+    node5->data = 5;
+    node5->left = NULL;
+    node5->right = (TreeNode *)malloc(sizeof(TreeNode));
+
+    TreeNode *node6 = node5->right;
+    node6->data = 6;
+    node6->left = NULL;
+    node6->right = NULL;
+    return node1;
+}
+void flatten(TreeNode *root)
+{
+    TreeNode *temp ;
+
+    while(root) {
+        if(root->left) {
+            temp = root->left;
+            while(temp->right) {
+                temp = temp->right;
+            }
+            temp->right = root->right;
+            root->right = root->left;
+            root->left = NULL;
+        }
+        root = root->right;
+    }
+}
 
 int main()
 {
+    TreeNode *head = CreateTestTree();
+    flatten(head);
+    printf("%d->%d->%d->%d->%d->%d\n",head->data,
+           head->right->data,
+           head->right->right->data,
+           head->right->right->right->data,
+           head->right->right->right->right->data,
+           head->right->right->right->right->right->data);
     return 0;
 }
